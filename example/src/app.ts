bcd94312c02b52cedec36d0ec709e265d3114d55
npmstudy <npmstudy@qq.com>
2023-09-29 21:15:38 +0800

    fix: remove react from example

--- example/src/app.ts
+++ example/src/app.ts
@@ -1,9 +1,9 @@
 import Koa from 'koa';
-
+import { lib } from 'lib2';
 const app = new Koa();
 
 app.use(async (ctx) => {
-  ctx.body = 'Helsslo World';
+  ctx.body = `Hello ${lib()}`;
 });
 
 app.listen(3000);
